8e3e90e1a9acf97ded947f703852c485ccca7be6
Sibel Yıldız <sibel_yldz@yahoo.com>
2024-12-10 20:27:06 +0300

    Add files via upload

---
+++ Film_Api_Example.py
@@ -0,0 +1,23 @@
+import requests
+
+api_key = "97b9d3a2"
+base_url = "https://www.omdbapi.com/"
+
+movie_name = input("Tell me the name of the movie you want to watch: ")
+
+
+params = {
+    "apikey": api_key,
+    "s": movie_name  
+}
+
+response = requests.get(base_url, params=params)
+
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
+
+    
